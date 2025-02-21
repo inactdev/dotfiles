5a22ef3405aa364ff5ad80ab43e634845198ecc3
Ari P <10625312+inactdev@users.noreply.github.com>
2025-02-21 17:52:04 +0000

    Final configs hopefully

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,6 +3,6 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>m", ":Explore<CR>", { desc = "Go to file explorer" })
+keymap.set("n", "<leader>m", ":Oil<CR>", { desc = "Go to file explorer" })
 
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,27 @@ return {
           auto_install = true,
           highlight = { enable = true },
           indent = { enable = false },
+          -- ensure these language parsers are installed
+          ensure_installed = {
+            "ruby",
+            "json",
+            "javascript",
+            "typescript",
+            "tsx",
+            "yaml",
+            "html",
+            "css",
+            "markdown",
+            "markdown_inline",
+            "graphql",
+            "bash",
+            "lua",
+            "vim",
+            "dockerfile",
+            "gitignore",
+            "query",
+            "vimdoc",
+          },
         })
       end
     }
