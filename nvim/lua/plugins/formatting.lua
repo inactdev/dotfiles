f656774f0984018177fd4d19bd0fb80fd0ecaa6f
Ari P <10625312+inactdev@users.noreply.github.com>
2025-06-22 17:34:18 +0000

    Fixes

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -6,32 +6,29 @@ return {
   
       conform.setup({
         formatters_by_ft = {
-          ruby = { "prettier" },
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          javascriptreact = { "prettier" },
-          typescriptreact = { "prettier" },
-          css = { "prettier" },
-          html = { "prettier" },
-          json = { "prettier" },
-          yaml = { "prettier" },
-          markdown = { "prettier" },
-          graphql = { "prettier" },
+          ruby = { "prettierd", "prettier" },
+          javascript = { "prettierd", "prettier" },
+          typescript = { "prettierd", "prettier" },
+          javascriptreact = { "prettierd", "prettier" },
+          typescriptreact = { "prettierd", "prettier" },
+          css = { "prettierd", "prettier" },
+          html = { "prettierd", "prettier" },
+          json = { "prettierd", "prettier" },
+          yaml = { "prettierd", "prettier" },
+          markdown = { "prettierd", "prettier" },
           lua = { "stylua" },
-          python = { "isort", "black" },
+python = { "isort", "black" },
         },
         format_on_save = {
+          timeout_ms = 500,
           lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
         },
       })
   
       vim.keymap.set({ "n", "v" }, "<leader>mp", function()
         conform.format({
           lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
+          async = true,
         })
       end, { desc = "Format file or range (in visual mode)" })
     end,
