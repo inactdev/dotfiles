d222687ece885aa4704067136d099c0364bcf41c
Aristides Perez <inactdev@Aristidess-MacBook-Pro.local>
2026-01-02 13:32:59 -0500

    Fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,44 @@
 return {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-          auto_install = true,
-          highlight = { enable = true },
-          indent = { enable = false },
-          -- ensure these language parsers are installed
-          ensure_installed = {
-            "ruby",
-            "json",
-            "javascript",
-            "typescript",
-            "tsx",
-            "yaml",
-            "html",
-            "css",
-            "markdown",
-            "markdown_inline",
-            "graphql",
-            "bash",
-            "lua",
-            "vim",
-            "dockerfile",
-            "gitignore",
-            "query",
-            "vimdoc",
-          },
-        })
-      end
-    }
-  }
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local ts = require("nvim-treesitter")
+
+		-- Install core parsers (add your languages here)
+		ts.install({
+			"ruby",
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+		}, { summary = false }):wait(30000)
+
+		-- Autocmd to enable Treesitter on file open
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter_enable", { clear = true }),
+			pattern = "*",
+			callback = function(event)
+				local lang = event.match
+				local ok, task = pcall(ts.install, { lang }, { summary = true })
+				if ok then
+					task:wait(10000)
+				end
+				pcall(vim.treesitter.start, event.buf, lang)
+			end,
+		})
+	end,
+}
