b68f3557c602a4300e579f150e898046a939c8e6
Ari <aris.a.perez@gmail.com>
2026-07-30 00:06:58 -0400

    Fix wezterm.lua filename typo

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,11 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("ayu").setup({})
-		vim.cmd.colorscheme("ayu")
+		require("ayu").setup({
+      mirage = true, -- Use mirage instead of dark for dark background
+      terminal = true, -- Let terminal manage its own colors if false
+      overrides = {} -- Customize specific highlights
+    })
+		vim.cmd.colorscheme("ayu-mirage")
 	end,
 }
